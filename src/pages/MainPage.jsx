93c5df488b2203d6018754aa32f23292067b1733
Seu nome <senamatheus.job@gmail.com>
2022-02-15 11:12:04 -0300

    MainPage done

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import '../css/MainPage.css';
+import { useHistory } from 'react-router-dom';
+import ash from '../images/ash.png';
+import girl from '../images/girl.png';
+import title from '../images/title.png';
 
 export default function MainPage() {
+  const history = useHistory();
+
   return (
-    <div>MainPage</div>
+    <div className="MainPage">
+      <img className="title-image" src={ title } alt="title" />
+      <div className="main-button-container">
+        <button type="button" onClick={ () => history.push('/kanto') }>
+          Kanto
+        </button>
+        <button type="button" onClick={ () => history.push('/johto') }>
+          Johto
+        </button>
+        <button type="button" onClick={ () => history.push('/hoenn') }>
+          Hoenn
+        </button>
+        <button type="button" onClick={ () => history.push('/sinnoh') }>
+          Sinnoh
+        </button>
+        <button type="button" onClick={ () => history.push('/unova') }>
+          Unova
+        </button>
+        <button type="button" onClick={ () => history.push('/favorites') }>
+          Favorites
+        </button>
+      </div>
+      <div className="main-image-container">
+        <img className="ash" src={ ash } alt="ash" />
+        <img className="girl" src={ girl } alt="girl" />
+      </div>
+    </div>
   );
 }
