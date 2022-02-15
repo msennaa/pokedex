90daa47501c771e908eed00216273d33bd195290
Seu nome <senamatheus.job@gmail.com>
2022-02-15 12:37:10 -0300

    Pages Hoenn, Johto, Sinnoh and Unova

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PokedexProvider from './context/PokedexProvider';
+import Johto from './pages/Johto';
 import Kanto from './pages/Kanto';
+import Hoenn from './pages/Hoenn';
+import Sinnoh from './pages/Sinnoh';
+import Unova from './pages/Unova';
 import MainPage from './pages/MainPage';
 
 export default function App() {
@@ -11,6 +15,10 @@ export default function App() {
         <Switch>
           <Route path="/" exact component={ MainPage } />
           <Route path="/kanto" exact component={ Kanto } />
+          <Route path="/johto" exact component={ Johto } />
+          <Route path="/hoenn" exact component={ Hoenn } />
+          <Route path="/sinnoh" exact component={ Sinnoh } />
+          <Route path="/unova" exact component={ Unova } />
         </Switch>
       </BrowserRouter>
     </PokedexProvider>
